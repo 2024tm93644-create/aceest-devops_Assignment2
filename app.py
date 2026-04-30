baf4bfc7356f40846d1b8fea9357d15365a04a05
amitoshgautam <singh259@gmail.com>
2026-04-30 22:39:20 +0530

    Updated code for deployement stretgy

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
+import random
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "ACEest Fitness DevOps Application Running - Version 2"
+    return "ACEest Fitness DevOps Application Running"
 
-@app.route("/health")
-def health():
-    return {"status": "ok"}
-
-@app.route('/members')
-def members():
-    return "Members list - Version 2"
+@app.route("/ab")
+def ab_test():
+    version = random.choice(["A", "B"])
+    
+    if version == "A":
+        return "Version A - Basic UI"
+    else:
+        return "Version B - Improved UI"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
